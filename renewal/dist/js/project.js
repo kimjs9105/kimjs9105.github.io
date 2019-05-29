1485bd2414f52cf4237c8f6de36a0a6d1b80a6b1
JSun <ls2022@naver.com>
2019-05-29 15:12:21 +0900

    2019.05.29 - 포트폴리오 스크롤 작업중
    포트폴리오 슬라이드 페이지 제작 중(스크립트 작업 진행 중)

--- renewal/dist/js/project.js
+++ renewal/dist/js/project.js
@@ -1,5 +1,3 @@
-console.log("hi");
-
 var project2016 = [
   {
     logo: "",
@@ -84,7 +82,7 @@ var project2017 = [
     part : "퍼블리싱 100%",
     detail : ""
   }
-
+];
 
   var project2018 = [
     {
@@ -106,8 +104,7 @@ var project2017 = [
     {
       logo: "",
       brand : "로레알프로페셔널",
-      summary : "글로벌 대표 화장품 '로레알'의 대표 헤어 전문 브랜드인 '로레알 프로페셔널 파리'의 브랜드 사이트 리뉴얼 프로젝트를 진행 했습니다.
-                ",
+      summary : "글로벌 대표 화장품 '로레알'의 대표 헤어 전문 브랜드인 '로레알 프로페셔널 파리'의 브랜드 사이트 리뉴얼 프로젝트를 진행 했습니다.",
       period : "",
       part : "퍼블리싱 100%",
       detail : ""
@@ -115,8 +112,7 @@ var project2017 = [
     {
       logo: "",
       brand : "샤넬",
-      summary : "남녀노소 모두 사용 가능한 스킨케어 제품군을 보유하고 있는 니베아의 오픈 마켓(G마켓, 11번가 등) 퍼블리싱을 진행하였습니다.
-                주로 이미지 더미 형태의 레이아웃으로 퍼블리싱 작업을 진행 하였습니다.",
+      summary : "남녀노소 모두 사용 가능한 스킨케어 제품군을 보유하고 있는 니베아의 오픈 마켓(G마켓, 11번가 등) 퍼블리싱을 진행하였습니다. 주로 이미지 더미 형태의 레이아웃으로 퍼블리싱 작업을 진행 하였습니다.",
       period : "2018.06 ~ 10 (약 5개월)",
       part : "퍼블리싱 100%",
       detail : ""
@@ -124,8 +120,7 @@ var project2017 = [
     {
       logo: "",
       brand : "니베아",
-      summary : "남녀노소 모두 사용 가능한 스킨케어 제품군을 보유하고 있는 니베아의 오픈 마켓(G마켓, 11번가 등) 퍼블리싱을 진행하였습니다.
-                주로 이미지 더미 형태의 레이아웃으로 퍼블리싱 작업을 진행 하였으며 탭을 사용하여 다른 섹션으로 이동하는 모션을 주었습니다.",
+      summary : "남녀노소 모두 사용 가능한 스킨케어 제품군을 보유하고 있는 니베아의 오픈 마켓(G마켓, 11번가 등) 퍼블리싱을 진행하였습니다. 주로 이미지 더미 형태의 레이아웃으로 퍼블리싱 작업을 진행 하였으며 탭을 사용하여 다른 섹션으로 이동하는 모션을 주었습니다.",
       period : "2018.10 ~ 현재",
       part : "퍼블리싱 100%",
       detail : ""
@@ -133,10 +128,7 @@ var project2017 = [
     {
       logo: "",
       brand : "샌텔 홈페이지",
-      summary : "샌텔 홈페이지 리뉴얼 작업을 진행하였습니다.
-                기존의 홈페이지는 단일 페이지 형태 사이트로써 샌텔의 아이덴티티와 프로젝트의 내용을 모두 반영시키기엔 부족하다고 판단하였습니다
-                따라서 신규 홈페이지는 프로젝트 별로 개별페이지를 만들어 제작하였으며
-                반응형으로 제작함으로써 모바일에서도 자유롭게 접속이 가능하도록 리뉴얼 하였습니다. ",
+      summary : "샌텔 홈페이지 리뉴얼 작업을 진행하였습니다. 기존의 홈페이지는 단일 페이지 형태 사이트로써 샌텔의 아이덴티티와 프로젝트의 내용을 모두 반영시키기엔 부족하다고 판단하였습니다 따라서 신규 홈페이지는 프로젝트 별로 개별페이지를 만들어 제작하였으며 반응형으로 제작함으로써 모바일에서도 자유롭게 접속이 가능하도록 리뉴얼 하였습니다. ",
       period : "2018.01 ~ 02 (약 2개월)",
       part : "퍼블리싱 100%",
       detail : ""
@@ -144,10 +136,7 @@ var project2017 = [
     {
       logo: "",
       brand : "리복 제안서 코딩",
-      summary : "리복 홈페이지 리뉴얼 관련 제안서 코딩을 진행하였습니다.(제안서 코딩으로 해당 페이지는 더미 작업을 진행하였습니다.)
-                기존의 홈페이지는 해상도가 낮은 모니터에 맞추어 제작 되었기 때문에 답답한 느낌이 강하였습니다
-                이러한 부분을 해결하기 위하여 신규 제안 홈페이지는 풀페이지 배너 및 섹션을 사용하여 보다 시원한 느낌을 주었으며
-                다양한 모션을 통해 스포츠브랜드의 역동성을 표현해보았습니다.",
+      summary : "리복 홈페이지 리뉴얼 관련 제안서 코딩을 진행하였습니다.(제안서 코딩으로 해당 페이지는 더미 작업을 진행하였습니다.) 기존의 홈페이지는 해상도가 낮은 모니터에 맞추어 제작 되었기 때문에 답답한 느낌이 강하였습니다 이러한 부분을 해결하기 위하여 신규 제안 홈페이지는 풀페이지 배너 및 섹션을 사용하여 보다 시원한 느낌을 주었으며 다양한 모션을 통해 스포츠브랜드의 역동성을 표현해보았습니다.",
       period : "2018.02 (약 2주)",
       part : "퍼블리싱 100%",
       detail : ""
@@ -158,8 +147,7 @@ var project2017 = [
     {
       logo: "",
       brand : "애큐온 저축은행",
-      summary : "애큐온 저축은행의 직원들이 사용하는 금융 ERP 시스템의 리뉴얼 프로젝트를 진행하였습니다
-                 깔끔한 UI와 페이지를 한눈에 볼수 있는 레이아웃 구조에 중점을 두었습니다.",
+      summary : "애큐온 저축은행의 직원들이 사용하는 금융 ERP 시스템의 리뉴얼 프로젝트를 진행하였습니다 깔끔한 UI와 페이지를 한눈에 볼수 있는 레이아웃 구조에 중점을 두었습니다.",
       period : "2019.01 ~ 02 (약 2개월)",
       part : "퍼블리싱 100%",
       detail : ""
@@ -197,3 +185,79 @@ var project2017 = [
     //   detail : ""
     // }
   ];
+
+  $(document).ready(function(){
+
+    console.log('hi');
+
+      $("#slideCon").on('mousewheel', function(e){
+
+        var $actSection = $('#slideCon').find('.on').index();
+        var $pofolSection = $('#slideCon').children('div').length;
+
+        if( e.deltaY < 0 ){
+          // console.log($actSection);
+          // console.log($pofolSection);
+          //
+          // scrollMainCon.scrollDown($actSection,$pofolSection);
+          if($actSection + 1 === $pofolSection){
+            alert('마지막 프로젝트 입니다.');
+          }else{
+            console.log('프로젝트 : ' + $actSection);
+
+            $('#slideCon').children('div').removeClass('on');
+            $('#slideCon').children('div').find('button').removeClass('act');
+
+            $('#slideCon').children('div').eq($actSection + 1).addClass('on').css({ left: '100%'});
+            $('#slideCon').children('div').stop().eq($actSection).animate({left: '-100%'}, 500);
+            $('#slideCon').children('div').stop().eq($actSection + 1).animate({left: 0}, 500, function(){
+              $(this).find('button').addClass('act');
+            });
+          }
+
+        }else{
+          scrollMainCon.scrollUp($actSection,$pofolSection);
+        }
+      });
+  });
+
+  var scrollMainCon = {
+
+    scrollDown : function(index,lastI){
+
+      if(index + 1 === lastI){
+        alert('마지막 프로젝트 입니다.');
+
+      }else{
+        console.log('프로젝트 : ' + index);
+
+        $('#slideCon').children('div').removeClass('on');
+        $('#slideCon').children('div').find('button').removeClass('act');
+
+        $('#slideCon').children('div').eq(index + 1).addClass('on').css({ left: '100%'});
+        $('#slideCon').children('div').stop().eq(index).animate({left: '-100%'}, 500);
+        $('#slideCon').children('div').stop().eq(index + 1).animate({left: 0}, 500, function(){
+          $(this).find('button').addClass('act');
+        });
+      }
+    },
+
+    scrollUp : function(index,lastI){
+
+      if(index === 0){
+        alert('첫 프로젝트 입니다.');
+
+      }else{
+        console.log('프로젝트 : ' + index);
+
+        $('#slideCon').children('div').removeClass('on');        
+        $('#slideCon').children('div').find('button').removeClass('act');
+
+        $('#slideCon').children('div').eq(index - 1).addClass('on').css({ left: '-100%'});
+        $('#slideCon').children('div').stop().eq(index).animate({left: '100%'}, 500);
+        $('#slideCon').children('div').stop().eq(index - 1).animate({left: 0}, 500, function(){
+          $(this).find('button').addClass('act');
+        });
+      }
+    }
+  }
